dd8cb047844e774af09c48b0f8830873e715e130
JUSTAWA7 <amirbekjumanazarov167@gmail.com>
2026-07-19 13:50:46 +0300

    Use environment secret key

--- online_market/settings.py
+++ online_market/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b_@iq6&$m#_$683*l89@r14!mg=7_2he+ac_erqxka-ldr$%)r'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
