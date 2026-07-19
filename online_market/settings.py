10482f08a605b39c56785821c7b0bd03633bffef
JUSTAWA7 <amirbekjumanazarov167@gmail.com>
2026-07-19 14:10:14 +0300

    Fix SECRET_KEY environment

--- online_market/settings.py
+++ online_market/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-b_@iq6&$m#_$683*l89@r14!mg=7_2he+ac_erqxka-ldr$%)r')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
